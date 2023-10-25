e4f6089a665509102f42393077196634ccdbbcea
Tarlispace69 <116681079+Tarlispace69@users.noreply.github.com>
2023-10-25 13:23:57 +0300

    For-6-Ron complete

---
+++ ConsoleApp183/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp183
+{
+    class Program
+    {
+        const int MEMBERS = 10;
+        static void Main(string[] args)
+        {
+            int i = 0, totalAmount = 0;
+
+            //receiving amount given by each member
+            for (i=0; i < MEMBERS; i++)
+            {
+                Console.WriteLine("HOW MUCH MONEY ARE YOU GIVING????? (in shekels)");
+                totalAmount += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Total amount: {totalAmount}");
+            Console.ReadLine();
+        }
+    }
+}
